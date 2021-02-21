522ea80afb373b7d25807995d6f1529aa5dd3f29
gouthamelango <goutham4elango@gmail.com>
2021-02-22 00:50:07 +0530

    HomePage --UPDATED--

--- app/src/main/java/com/example/hardbank/HomeFragment.java
+++ app/src/main/java/com/example/hardbank/HomeFragment.java
@@ -1,9 +1,13 @@
 package com.example.hardbank;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,9 +15,20 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageClickListener;
 import com.synnapps.carouselview.ImageListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -35,6 +50,22 @@ public class HomeFragment extends Fragment {
     CarouselView carouselView;
 
     int[] sampleImages = {R.drawable.dummycomponentimage,R.drawable.dummycomponentimage,R.drawable.dummycomponentimage,R.drawable.dummycomponentimage,R.drawable.dummycomponentimage};
+    List<String> images = new ArrayList<>();
+
+
+    FirebaseAuth mAuth;
+    FirebaseFirestore db;
+
+    private HomeProductAdapter adapter;
+    RecyclerView recyclerView;
+    RecyclerView recyclerViewTools;
+
+    List<String> productsID = new ArrayList<>();
+
+    Context context;
+
+    List<Product> products =new ArrayList<>();
+    List<Product> productsTools =new ArrayList<>();
 
     public HomeFragment() {
         // Required empty public constructor
@@ -82,9 +113,14 @@ public class HomeFragment extends Fragment {
             }
         });
 
+        images.add("https://whatis6g.com/wp-content/uploads/2020/08/internet-of-senses-inset-1080x675.jpg");
+        images.add("https://searchengineland.com/figz/wp-content/seloads/2017/10/internet-of-things-smart-ss-1920.gif");
+        images.add("https://www.mobifilia.com/wp-content/uploads/2018/11/IoT-for-Healthcare.jpg");
+        images.add("https://innovationatwork.ieee.org/wp-content/uploads/2017/06/bigstock-185933284.jpg");
+
         //CarouselView
         carouselView = (CarouselView) view.findViewById(R.id.carousel);
-        carouselView.setPageCount(sampleImages.length);
+        carouselView.setPageCount(images.size());
         carouselView.setImageListener(imageListener);
         carouselView.setImageClickListener(new ImageClickListener() {
             @Override
@@ -94,12 +130,114 @@ public class HomeFragment extends Fragment {
             }
         });
 
+        //Firebase initialization
+        mAuth = FirebaseAuth.getInstance();
+        db =  FirebaseFirestore.getInstance();
+
+        //Getting my products RecyclerView for sensors
+        recyclerView = view.findViewById(R.id.sensorRecyclerView);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this.getActivity(),LinearLayoutManager.HORIZONTAL,false);
+        recyclerView.setLayoutManager(mLayoutManager);
+
+        //Getting my products RecyclerView for tools
+        recyclerViewTools = view.findViewById(R.id.toolsRecyclerView);
+        RecyclerView.LayoutManager mLayoutManagerTools = new LinearLayoutManager(this.getActivity(),LinearLayoutManager.HORIZONTAL,false);
+        recyclerViewTools.setLayoutManager(mLayoutManagerTools);
+
+        context  = getActivity().getApplicationContext();
+
+        setUpRecyclerView();
+        setUpRecyclerViewForTools();
+
         return view;
     }
+
+    private void setUpRecyclerView(){
+        db.collection("products").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    QuerySnapshot queryDocumentSnapshots = task.getResult();
+                    List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+                    for (int i = 0; i < list.size(); i++) {
+                        DocumentSnapshot doc=list.get(i);
+                        if(doc.getString("verified").equals("true")){
+                            if(doc.getString("category").equals("Sensors")){
+                                //Toast.makeText(getActivity().getApplicationContext(),doc.getString("productname"),Toast.LENGTH_SHORT).show();
+                                String productname = doc.getString("productname");
+                                String productprice = doc.getString("productprice");
+                                //Toast.makeText(getApplicationContext(),productname,Toast.LENGTH_SHORT).show();
+                                String category = doc.getString("category");
+                                String id = doc.getString("id");
+                                String image = doc.getString("image");
+                                String productbrand = doc.getString("productbrand");
+                                String productdeliveryprice = doc.getString("productdeliveryprice");
+                                String productdescription = doc.getString("productdescription");
+                                String  reason = doc.getString("reason");
+                                String verified = doc.getString("verified");
+                                Product product = new Product(productname, productprice,category,id, image,productbrand,
+                                        productdeliveryprice, productdescription, verified, reason);
+                                products.add(product);
+                                adapter = new HomeProductAdapter(context,products);
+                                //Toast.makeText(getActivity().getApplicationContext(),product.getImage(),Toast.LENGTH_SHORT).show();
+                                recyclerView.setAdapter(adapter);
+                            }
+                        }
+
+                    }
+
+                }
+            }
+        });
+    }
+    private void setUpRecyclerViewForTools(){
+        db.collection("products").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    QuerySnapshot queryDocumentSnapshots = task.getResult();
+                    List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+                    for (int i = 0; i < list.size(); i++) {
+                        DocumentSnapshot doc=list.get(i);
+                        if(doc.getString("verified").equals("true")){
+                            if(doc.getString("category").equals("Tools")){
+                                //Toast.makeText(getActivity().getApplicationContext(),doc.getString("productname"),Toast.LENGTH_SHORT).show();
+                                String productname = doc.getString("productname");
+                                String productprice = doc.getString("productprice");
+                                //Toast.makeText(getApplicationContext(),productname,Toast.LENGTH_SHORT).show();
+                                String category = doc.getString("category");
+                                String id = doc.getString("id");
+                                String image = doc.getString("image");
+                                String productbrand = doc.getString("productbrand");
+                                String productdeliveryprice = doc.getString("productdeliveryprice");
+                                String productdescription = doc.getString("productdescription");
+                                String  reason = doc.getString("reason");
+                                String verified = doc.getString("verified");
+                                Product product = new Product(productname, productprice,category,id, image,productbrand,
+                                        productdeliveryprice, productdescription, verified, reason);
+                                productsTools.add(product);
+                                adapter = new HomeProductAdapter(context,productsTools);
+                                //Toast.makeText(getActivity().getApplicationContext(),product.getImage(),Toast.LENGTH_SHORT).show();
+                                recyclerViewTools.setAdapter(adapter);
+                            }
+                        }
+
+                    }
+
+                }
+            }
+        });
+    }
+
     ImageListener imageListener = new ImageListener() {
         @Override
         public void setImageForPosition(int position, ImageView imageView) {
-            imageView.setImageResource(sampleImages[position]);
+            //imageView.setImageResource(sampleImages[position]);
+
+                Glide.with(imageView.getContext())
+                        .load(images.get(position))
+                        .into(imageView);
+
         }
     };
 }
---
+++ app/src/main/java/com/example/hardbank/HomeProductAdapter.java
@@ -0,0 +1,74 @@
+package com.example.hardbank;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class HomeProductAdapter extends RecyclerView.Adapter<HomeProductAdapter.MyViewHolder> {
+
+    Context context;
+    List<Product> list;
+
+    public HomeProductAdapter(Context context,List<Product> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+        view= LayoutInflater.from(parent.getContext()).inflate(R.layout.home_catgeory_product_single_view,parent,false);
+        return new HomeProductAdapter.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.textViewProductName.setText(list.get(position).getProductname());
+        holder.textViewProductBrand.setText(list.get(position).getProductbrand());
+        Glide.with(holder.imageViewProductImg.getContext())
+                .load(list.get(position).getImage())
+                .into(holder.imageViewProductImg);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(),"Clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.textViewProductPrice.setText(list.get(position).getProductprice());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public   class MyViewHolder extends RecyclerView.ViewHolder{
+        ImageView imageViewProductImg;
+        TextView textViewProductName;
+        TextView textViewProductBrand;
+        TextView textViewProductPrice;
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewProductName = itemView.findViewById(R.id.productName);
+            textViewProductBrand = itemView.findViewById(R.id.productBrand);
+            textViewProductPrice = itemView.findViewById(R.id.productPrice);
+
+            imageViewProductImg  = itemView.findViewById(R.id.productImage);
+        }
+    }
+}
