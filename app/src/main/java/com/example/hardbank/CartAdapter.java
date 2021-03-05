fbd0d1fc780edba0e5352ed629428c81fbdf999b
gouthamelango <goutham4elango@gmail.com>
2021-03-06 02:02:44 +0530

    Cart --UPDATED--

--- app/src/main/java/com/example/hardbank/CartAdapter.java
+++ app/src/main/java/com/example/hardbank/CartAdapter.java
@@ -17,7 +17,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -39,7 +42,7 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.MyViewHolder> 
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
+    public void onBindViewHolder(@NonNull final MyViewHolder holder, final int position) {
 
         holder.productNameTextView.setText(list.get(position).getProductname());
         holder.productPrice.setText(list.get(position).getProductprice());
@@ -47,6 +50,71 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.MyViewHolder> 
         Glide.with(holder.productImage.getContext())
                 .load(list.get(position).getImage())
                 .into(holder.productImage);
+
+        holder.price =  Integer.parseInt(list.get(position).getProductprice());
+
+        FirebaseFirestore.getInstance().collection("users").document(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .collection("cart").document(list.get(position).getId()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                holder.estimatedPrice.setText(modifyPrice(holder.price,Integer.parseInt(documentSnapshot.getString("quantity"))));
+                holder.textViewQuantity.setText(documentSnapshot.getString("quantity"));
+            }
+        });
+
+
+        holder.selectQuantityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+
+
+                FirebaseFirestore.getInstance().collection("users").document(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                        .collection("cart").document(list.get(position).getId()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        AlertDialog.Builder alertDialog = new AlertDialog.Builder(view.getRootView().getContext());
+                        alertDialog.setTitle("Select Quantity");
+                        String[] items = {"1","2","3","4","5"};
+                        int checkedItem = Integer.parseInt( documentSnapshot.getString("quantity"))-1;
+                        alertDialog.setSingleChoiceItems(items, checkedItem, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                switch (which) {
+                                    case 0:
+                                        holder.estimatedPrice.setText(modifyPrice(holder.price,1));
+                                        holder.textViewQuantity.setText("1");
+                                        writeQuantity(1,position);
+                                        break;
+                                    case 1:
+                                        holder.estimatedPrice.setText(modifyPrice(holder.price,2));
+                                        holder.textViewQuantity.setText("2");
+                                        writeQuantity(2,position);
+                                        break;
+                                    case 2:
+                                        holder.estimatedPrice.setText(modifyPrice(holder.price,3));
+                                        holder.textViewQuantity.setText("3");
+                                        writeQuantity(3,position);
+                                        break;
+                                    case 3:
+                                        holder.estimatedPrice.setText(modifyPrice(holder.price,4));
+                                        holder.textViewQuantity.setText("4");
+                                        writeQuantity(4,position);
+                                        break;
+                                    case 4:
+                                        holder.estimatedPrice.setText(modifyPrice(holder.price,5));
+                                        holder.textViewQuantity.setText("5");
+                                        writeQuantity(5,position);
+                                        break;
+                                }
+                            }
+                        });
+                        AlertDialog alert = alertDialog.create();
+                        alert.show();
+                      }
+                });
+            }
+        });
+
 
         holder.removeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,6 +152,18 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.MyViewHolder> 
 
     }
 
+    public String modifyPrice(int price, int quantity){
+
+        return  String.valueOf(price*quantity);
+    }
+    public  void writeQuantity(int quantity, int position){
+        FirebaseFirestore.getInstance().collection("users").document(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .collection("cart").document(list.get(position).getId()).update("quantity",String.valueOf(quantity));
+
+    }
+
+
+
     @Override
     public int getItemCount() {
         return list.size();
@@ -96,6 +176,11 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.MyViewHolder> 
         TextView productPrice;
         TextView productBrand;
         TextView removeBtn;
+        TextView estimatedPrice;
+        TextView textViewQuantity;
+        int price;
+
+        RelativeLayout selectQuantityBtn;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -104,6 +189,9 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.MyViewHolder> 
             productPrice = itemView.findViewById(R.id.productPrice);
             productBrand = itemView.findViewById(R.id.productBrand);
             removeBtn  =  itemView.findViewById(R.id.removeBtn);
+            estimatedPrice =  itemView.findViewById(R.id.estimatedPrice);
+            selectQuantityBtn = itemView.findViewById(R.id.selectQuantityBtn);
+            textViewQuantity =  itemView.findViewById(R.id.textViewQuantity);
         }
     }
 }
