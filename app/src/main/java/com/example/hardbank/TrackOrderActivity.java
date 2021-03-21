b903325c049d0751f447c01379f51398848b4328
gouthamelango <goutham4elango@gmail.com>
2021-03-22 00:02:51 +0530

    Order Delivery (QR Scanner) --UPDATED--

--- app/src/main/java/com/example/hardbank/TrackOrderActivity.java
+++ app/src/main/java/com/example/hardbank/TrackOrderActivity.java
@@ -6,6 +6,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -18,6 +19,8 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import java.util.Date;
 
@@ -42,6 +45,8 @@ public class TrackOrderActivity extends AppCompatActivity {
     String orderStatus;
 
     Button cancelBtn;
+
+    ImageView scanBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,6 +90,9 @@ public class TrackOrderActivity extends AppCompatActivity {
 
         cancelBtn =  findViewById(R.id.cancelBtn);
 
+
+        scanBtn = findViewById(R.id.scanBtn);
+
         Intent intent =  getIntent();
         if(intent.hasExtra("orderid")){
 
@@ -127,6 +135,8 @@ public class TrackOrderActivity extends AppCompatActivity {
                         estimatedDeliveryLayout.setVisibility(View.VISIBLE);
 
                         cancelBtn.setVisibility(View.VISIBLE);
+
+                        scanBtn.setVisibility(View.VISIBLE);
 
                     }
                     else  if(orderStatus.equals("Cancelled")){
@@ -191,9 +201,57 @@ public class TrackOrderActivity extends AppCompatActivity {
                 }
             });
 
+            scanBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                   // Toast.makeText(TrackOrderActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+                    IntentIntegrator integrator = new IntentIntegrator(TrackOrderActivity.this);
+                    integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                    integrator.setPrompt("Scan");
+                    integrator.setCameraId(0);
+                    integrator.setBeepEnabled(false);
+                    integrator.setBarcodeImageEnabled(false);
+                    integrator.initiateScan();
+                }
+            });
+
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Log.e("Scan*******", "Cancelled scan");
+
+            } else {
+                Log.e("Scan", "Scanned");
+
+                //tv_qr_readTxt.setText(result.getContents());
+                //Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                if(result.getContents().equals(orderID)){
+                  db.collection("orders").document(orderID).update("status","Delivered").addOnSuccessListener(new OnSuccessListener<Void>() {
+                      @Override
+                      public void onSuccess(Void aVoid) {
+                          Toast.makeText(getApplicationContext(), "Your Order Has been Delivered", Toast.LENGTH_LONG).show();
+                          Intent intent = getIntent();
+                          intent.putExtra("orderid",orderID);
+                          finish();
+                          startActivity(intent);
+                      }
+                  });
+                }
+                else {
+                    Toast.makeText(this, "Wrong QR Code", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        } else {
+            // This is important, otherwise the result will not be passed to the fragment
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
 
 
 
